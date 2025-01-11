43b969222710d86493458d694cc1777fb2284cd2
Afaaf Hariri <haririafaaf@gmail.com>
2025-01-11 20:31:52 +0530

    nav

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,84 +1,59 @@
-import { NavLink } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-import { useState } from "react";
-import FictionDropDown from "./FictionDropDown";
-import NonFictionDropDown from "./NonFictionDropDown";
+import { NavLink, useNavigate } from "react-router-dom";
+
+interface NavItems {
+  label: string;
+}
+
+const navItems: NavItems[] = [
+  { label: "Adventure" },
+  { label: "Mystery" },
+  { label: "Crime & Thriller" },
+  { label: "Horror" },
+  { label: "Life" },
+  { label: "Romance" },
+  { label: "Sci-fi" },
+  { label: "Art & Culture" },
+  { label: "Politics" },
+  { label: "Finance" },
+  { label: "Lifestyle" },
+  { label: "Science & Technology" },
+];
 
 const NavBar = () => {
   const navigate = useNavigate();
-
-  const [showFictionDropDown, setFictionDropDown] = useState(false);
-  const [showNonFictionDropDown, setNonFictionDropDown] = useState(false);
-
-  const handleOnClick = () => {
-    navigate("/login");
+  const handleSignInClick = () => {
+    navigate("/signin");
   };
+
   return (
     <>
-      <div className="bg-transparent mx-auto flex justify-between items-center py-5 px-10 border-b-1 border-gray-200 shadow-md">
-        <div className="text-slate-800 text-2xl font-bold">
-          <NavLink to="/">
-            book<span className="text-red-600">MASTER.</span>
-          </NavLink>
-        </div>
-        <ul className="flex space-x-14 list-none no-underline text-slate-800 font-semibold text-sm">
-          <li>
-            <NavLink
-              to="/"
-              className={({ isActive }) =>
-                isActive
-                  ? "text-red-600"
-                  : "hover:text-red-600 transition-all duration-300"
-              }
-            >
-              Home
-            </NavLink>
-          </li>
-          <li
-            className="relative hover:text-red-600 transition duration-300 focus:text-red-600"
-            onMouseEnter={() => {
-              setFictionDropDown(true);
-              setNonFictionDropDown(false);
-            }}
-            onMouseLeave={() => {
-              setFictionDropDown(false);
-            }}
-          >
-            Fiction
-          </li>
-          <li
-            className="relative hover:text-red-600 transition duration-300 focus:text-red-600"
-            onMouseEnter={() => {
-              setNonFictionDropDown(true);
-              setFictionDropDown(false);
-            }}
-          >
-            Non-fiction
-          </li>
-        </ul>
+      <div className="bg-white relative min-w-screen px-14 py-4 flex items-center justify-between">
+        <NavLink to="/">
+          <p className="text-blue-900 font-bold text-3xl">
+            bookmaster<span className="text-red-600">.lk</span>
+          </p>
+        </NavLink>
+        <p className="text-2xl">Search</p>
         <button
-          className="border-1 shadow-md rounded-md py-2 px-4 text-sm font-bold text-slate-800 hover:text-red-600 hover:bg-red-50 transition-all duration-300"
-          onClick={handleOnClick}
+          onClick={handleSignInClick}
+          className="py-2 px-4 bg-white shadow-md shadow-slate-300 hover:bg-red-50 hover:text-red-600 text-sm rounded-md transition-all duration-300
+          font-semibold"
         >
           Sign In
         </button>
       </div>
-      {showFictionDropDown && (
-        <div
-          className="bg-transparent flex flex-col justify-between space-x-11 items-start mx-auto px-6 py-4 border-1 border-gray-200 shadow-md w-36 h-auto z-10"
-          onMouseLeave={() => setFictionDropDown(false)}
-        >
-          {<FictionDropDown />}
-        </div>
-      )}
-      {showNonFictionDropDown && (
-        <div
-          className="bg-transparent flex flex-col justify-between space-x-11 items-start mx-auto px-6 py-4 border-1 border-gray-200 shadow-md w-36 h-auto z-10"
-          onMouseLeave={() => setNonFictionDropDown(false)}
-        >
-          {<NonFictionDropDown />}
-        </div>
-      )}
+      <div className="bg-white relative min-w-screen px-14 py-4 flex items-center justify-between shadow-md shadow-slate-300">
+        <ul className="flex space-x-8 text-sm text-blue-900 mx-auto">
+          {navItems.map((item) => (
+            <li
+              className="hover:text-red-600 transition-all duration-500 font-medium"
+              key={item.label}
+            >
+              {item.label}
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 };
