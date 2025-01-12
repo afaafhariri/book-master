1ffa624ee449ef5727db57a56203ac039b2cb556
Afaaf Hariri <haririafaaf@gmail.com>
2025-01-12 15:37:03 +0530

    book model

--- server/models/events.ts
+++ server/models/events.ts
@@ -1,31 +1,41 @@
-import { Schema, Document } from "mongoose";
+import { Schema, Document, model } from "mongoose";
 
-const TicketSchema = new Schema({
-  price: { type: Number, required: true },
-  quantity: { type: Number, required: true },
-  category: { type: String, required: true },
-});
+interface Book extends Document {
+  name: string;
+  author: {
+    firstname: string;
+    middlename?: string;
+    lastname?: string;
+  };
+  price: number;
+  genre: string;
+  publisher: string;
+  isbn: string;
+  year: number;
+  views: number;
+  inStock: number;
+}
 
-const AddressSchema = new Schema({
-  street: { type: String, required: true },
-  city: { type: String, required: true },
-  state: { type: String, required: true },
-  postalCode: { type: String, required: true },
-  country: { type: String, required: true },
-});
-
-const EventSchema = new Schema(
+const BookSchema = new Schema(
   {
     name: { type: String, required: true },
-    description: { type: String, required: true },
-    eventcategory: { type: String, required: true },
-    date: { type: Date, required: true },
-    time: { type: String, required: true },
-    duration: { type: Number, required: false },
-    location: AddressSchema,
-    organizer: {},
-    tickets: [TicketSchema],
-    isVirtual: { type: Boolean, required: false },
+    author: {
+      firstname: { type: String, required: true },
+      middlename: { type: String, required: false },
+      lastname: { type: String, required: false },
+    },
+    price: { type: Number, required: true },
+    genre: { type: String, required: true },
+    publisher: { type: String, required: true },
+    isbn: { type: String, required: true },
+    year: { type: Number, required: true },
+    views: { type: Number, required: true },
+    inStock: { type: Number, required: true },
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+  }
 );
+
+const BookModel = model<Book>("Book", BookSchema);
+export default BookModel;
