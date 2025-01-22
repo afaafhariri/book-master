10eebd9dab0d70d85813fc16c9e79a504c81706a
Afaaf Hariri <haririafaaf@gmail.com>
2025-01-23 04:30:57 +0530

    css

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -48,44 +48,33 @@ const Home: React.FC = () => {
       <div className="py-8 px-40">
         <h2 className="text-blue-900 text-2xl font-semibold mb-4">Latest</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
-          {books
-            .slice(0, expanded ? books.length : 5) // Show all books if expanded, otherwise only 4
-            .map((book) => (
-              <div
-                key={book._id}
-                className="bg-white shadow-md rounded-lg overflow-hidden hover:shadow-lg hover:scale-95 transform transition-all duration-300"
-              >
-                <img
-                  src={book.image}
-                  alt={book.name}
-                  className="w-full h-48 object-contain bg-gray-100"
-                />
-                <div className="p-4">
-                  <p className="font-semibold text-gray-800">{book.name}</p>
-                  <p className="text-sm text-gray-600">
-                    by{" "}
-                    {`${book.author.firstname} ${book.author.lastname || ""}`}
-                  </p>
-                  <p className="text-sm text-gray-500 mt-2">
-                    Published on: {book.publish_date.toLocaleDateString()}
-                  </p>
-                  <p className="text-sm text-gray-500 mt-1">
-                    Genre: {book.genre}
-                  </p>
-                  <p className="text-sm text-gray-500 mt-1">
-                    Price: ${book.price}
-                  </p>
-                  <p className="text-sm text-gray-500 mt-1">
-                    Views: {book.views}
-                  </p>
-                </div>
-              </div>
-            ))}
+          {books.slice(0, expanded ? books.length : 5).map((book) => (
+            <div
+              key={book._id}
+              className="bg-white overflow-hidden p-4 hover:border border-gray-200 transition-all duration-300"
+            >
+              <img
+                src={book.image}
+                alt={book.name}
+                className="w-full h-48 object-contain bg-gray-100"
+              />
+              <p className="font-semibold text-gray-800 mt-4">{book.name}</p>
+              <p className="text-sm text-gray-600">
+                by {`${book.author.firstname} ${book.author.lastname || ""}`}
+              </p>
+              <p className="text-sm text-gray-500 mt-2">
+                Published on: {book.publish_date.toLocaleDateString()}
+              </p>
+              <p className="text-sm text-gray-500 mt-1">Genre: {book.genre}</p>
+              <p className="text-sm text-gray-500 mt-1">Price: ${book.price}</p>
+              <p className="text-sm text-gray-500 mt-1">Views: {book.views}</p>
+            </div>
+          ))}
         </div>
-        <div className="mt-6 text-center">
+        <div className="mt-6 text-center flex items-center justify-end">
           <button
             onClick={toggleExpanded}
-            className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition duration-300"
+            className="text-blue-900 font-medium px-4 py-2 border border-blue-900 hover:text-red-600 hover:border-red-600 transition-all duration-500"
           >
             {expanded ? "Show Less" : "Show More"}
           </button>
