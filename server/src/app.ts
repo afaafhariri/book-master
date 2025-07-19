f9fe65c82acdfa835f580c90d9c23aedb3e1d999
Afaaf Hariri <afaafhariri@icloud.com>
2025-07-19 23:18:14 +0530

    cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,12 +7,7 @@ import bookRouter from "./routes/bookRoutes";
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors());
 
 mongoose
   .connect(process.env.MONGO_URI as string)
